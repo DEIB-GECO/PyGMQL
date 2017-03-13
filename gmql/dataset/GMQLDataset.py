4de0a4f33116fea625049a7f35fbe32606b52086
lucananni93 <luca.nanni10@studio.unibo.it>
2017-03-13 18:05:39 +0100

    added meta_select at example1
    Former-commit-id: 87de4559221371d2981727b128ae508ae1c65146

--- gmql/dataset/GMQLDataset.py
+++ gmql/dataset/GMQLDataset.py
@@ -1,9 +1,14 @@
 from gmql import sc
 
 class GMQLDataset:
+    dataset = None
+
+    def __init__(self, dataset=None):
+        self.dataset = dataset
 
     def load_from_path(self, path, parser):
         rdd = sc.textFile(path).map(parser.parse_line)
-        return rdd
+        return GMQLDataset(dataset=rdd)
 
     def meta_select(self, predicate):
+        rdd = self.dataset.filter(predicate)
