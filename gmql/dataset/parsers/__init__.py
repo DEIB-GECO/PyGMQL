50ff1d202e34a9cc85b1fbf08d006e04aa93844d
lucananni93 <luca.nanni10@studio.unibo.it>
2017-05-30 12:49:12 +0200

    Added mapping between type_string and type
    Former-commit-id: ca6423ad27d06a050582e8f152677e6374bfbee8 [formerly 6cafe58cc99ad7887ad1005725ad0b5498f08b06] [formerly 9649e3e2fbff7738b06094a96927256310b102d8]
    Former-commit-id: 2cfc2f82926792698424ec8d46cef43cc8e0a7f8
    Former-commit-id: b2c040249781440c2f1365fb7897b410dc359f18

--- gmql/dataset/parsers/__init__.py
+++ gmql/dataset/parsers/__init__.py
@@ -1,2 +1,17 @@
 from .BedParser import BedParser
 from .NarrowPeakParser import NarrowPeakParser
+
+string_aliases = ['string', 'char']
+int_aliases = ['long', 'int', 'integer']
+float_aliases = ['double', 'float']
+
+
+def get_parsing_function(type_string):
+    if type_string in string_aliases:
+        return str
+    elif type_string in int_aliases:
+        return int
+    elif type_string in float_aliases:
+        return float
+    else:
+        raise ValueError("This type is not supported")
