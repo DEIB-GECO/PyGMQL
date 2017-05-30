7183edee23f715d225b7dc506746e3b7a96d7d6b
lucananni93 <luca.nanni10@studio.unibo.it>
2017-05-30 12:48:07 +0200

    Use only the base file name for key
    Former-commit-id: 3acbf9e93d3d501013e2a1b6aa9631bdcc663c66 [formerly eea949727d3693aa032033d84dcca3790d9072dd] [formerly ca065bc9f78b1416903179418d64b3273d437987]
    Former-commit-id: 58c1d18e8960ad3236a34b8080b18ccff5684eaa
    Former-commit-id: 6501068cea164df37ec055c3fb8baf8c89fc7823

--- gmql/dataset/loaders/__init__.py
+++ gmql/dataset/loaders/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Loader settings:
 we use the GMQL scala class CombineTextFileWithPathInputFormat in order to load the region and metadata files with the
@@ -16,11 +18,19 @@ conf = {"textinputformat.record.delimiter": "\n",
         }
 
 
+"""
+    Generation of the index of the pandas dataframe.
+    This can be done in different ways:
+    - hashing the complete file name
+    - using directly the file as index (this is visually appealing :) )
+"""
+
 def generateHashKey(filename):
     return hash(filename)
 
 
 def generateNameKey(filename):
+    filename = os.path.basename(filename)
     if filename.endswith(".meta"):
         return filename[:-5]
     else:
