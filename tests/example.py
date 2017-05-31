86685325f96bfab1cd8c081b66f582b85d705de0
lucananni93 <luca.nanni10@studio.unibo.it>
2017-05-31 18:56:34 +0200

    Tests for save of GDataframe in GMQL format
    Former-commit-id: 04948698ca8705c4945ce95a6d7abd3706b4d2e0 [formerly 20a6abd8ee75e71d15acfde76c43155a9b83da2e] [formerly 68cc24e6851bb1a9c368bf0f0a089f5ff1a9f49f]
    Former-commit-id: 0982a73aec32d3876c2958ff5500600583914787
    Former-commit-id: 28f7d68f2ccf91c5393a50bf2b82d4c3a9cd47e9

--- tests/example.py
+++ tests/example.py
@@ -3,9 +3,13 @@ import gmql as gl
 input_path = "/home/luca/Documenti/resources/hg_narrowPeaks_short/"
 
 parser = gl.parsers.NarrowPeakParser()
-
-gframe = gl.load_from_path(path=input_path, parser=parser,
-                           )
+dataset = gl.load_from_path(path=input_path, parser=parser, all_load=True)
+# dataset.reg_select((dataset.chr == 'chr9') & (dataset.score < 1))
+# result_gframe = dataset.materialize()
+#
+# print(result_gframe.regs.head())
+#
+# print(result_gframe.meta.head())
 
 print("DONE")
 
