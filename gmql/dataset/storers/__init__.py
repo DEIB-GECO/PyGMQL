429c59ae902a2470f31953bdabd524995e4b0e06
lucananni93 <luca.nanni10@studio.unibo.it>
2017-05-31 18:53:05 +0200

    Mapping between python types and Scala types

---
+++ gmql/dataset/storers/__init__.py
@@ -0,0 +1,14 @@
+pyType_to_scalaType = {
+    "object": "string",
+    "int64": "long",
+    "float64": "double",
+    "int32": "integer",
+    "float32": "double"
+}
+
+schema_header = "<?xml version='1.0' encoding='UTF-8'?>\n"
+schema_collection_template = "<gmqlSchemaCollection name= \"{}\" xmlns=\"http://genomic.elet.polimi.it/entities\">\n"
+schema_dataset_type_template = "\t<gmqlSchema type=\"{}\">\n"
+schema_field_template = "\t\t<field type=\"{}\">{}</field>\n"
+schema_dataset_end = "\t</gmqlSchema>\n"
+schema_collection_end = "</gmqlSchemaCollection>"
