b7455aee2945caa93ec53d414cdf85da149f09a1
lucananni93 <luca.nanni10@studio.unibo.it>
2017-05-31 18:54:40 +0200

    Enable the GDataframe save into the GMQL format
    Former-commit-id: 0fedbf1850a11986a4f91ecc8fd6223d6ac42ae7 [formerly a6a244b42b53dde96fc96fd240cacba6fa359108] [formerly 623050a136b37c10905d90f7d9dcb38c7f9e913d]
    Former-commit-id: f29f7ff7dd3d8cc025b436b61abb595fce730b3f
    Former-commit-id: 7ef8be7585b0702e092da76f036f1afb11c225b6

--- gmql/dataset/GDataframe.py
+++ gmql/dataset/GDataframe.py
@@ -1,3 +1,7 @@
+from .storers import FrameToGMQL
+import pandas as pd
+
+
 class GDataframe:
     """ Class holding the result of a materialization of a GMQLDataset.
     It is composed by two data structures:
@@ -6,12 +10,31 @@ class GDataframe:
         - A table with the *metadata* corresponding to the regions
     """
     def __init__(self, regs, meta):
+        if not isinstance(regs, pd.DataFrame):
+            raise TypeError("regs: expected pandas Dataframe, got {}".format(type(regs)))
+        if not isinstance(meta, pd.DataFrame):
+            raise TypeError("meta: expected pandas Dataframe, got {}".format(type(meta)))
+
         self.regs = regs
         self.meta = meta
 
-    def toGS(self):
+    def to_genomic_space(self):
         """ Translates the GDataframe to the Genomic Space data structure
         """
         raise NotImplementedError("Not yet implemented")
 
+    def to_dataset_files(self, local_path=None, remote_path=None):
+        """ Save the GDataframe to a local or remote location
 
+        :param local_path: a local path to the folder in which the data must be saved
+        :param remote_path: a remote dataset name that wants to be used for these data
+        :return: None
+        """
+        return FrameToGMQL.to_dataset_files(self, path_local=local_path, path_remote=remote_path)
+
+    def to_GMQLDataset(self):
+        """ Converts the GDataframe in a GMQLDataset for later local or remote computation
+
+        :return: a GMQLDataset
+        """
+        return None
