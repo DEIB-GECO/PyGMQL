562702c409457c297c2024d5f1324c2d9f5387fa
lucananni93 <luca.nanni10@studio.unibo.it>
2017-06-24 15:10:00 +0200

    from_pandas
    Former-commit-id: c19b1da58d81fb4733444b0227084192763f46e8 [formerly 94f9b188f54780d538099113c7174bad3b73327a] [formerly b3f8b6f32f31dc180d7db330ca5b2c46f76c6285]
    Former-commit-id: 0cf35462b4898cf398aeaad925c305ca5b4337df
    Former-commit-id: 88592936e469f85d995eeaa4d7e1c45630c5d6c8

--- gmql/dataset/GDataframe.py
+++ gmql/dataset/GDataframe.py
@@ -1,7 +1,14 @@
 from .storers import FrameToGMQL
 import pandas as pd
 from .loaders import Loader
+from .DataStructures import reg_fixed_fileds, \
+    strand_aliases, stop_aliases, start_aliases, chr_aliases
+import numpy as np
 
+chr_types = [object, str]
+start_types = [int, np.int, np.int8, np.int16, np.int32, np.int64]
+stop_types = start_types
+strand_types = [object, str]
 
 class GDataframe:
     """ Class holding the result of a materialization of a GMQLDataset.
@@ -44,3 +51,86 @@ class GDataframe:
             raise NotImplementedError()
         else:
             raise ValueError("You must specify at least one of local_path and remote_path")
+
+
+def from_pandas(regs, meta=None, chr_name=None, start_name=None, stop_name=None, strand_name=None):
+    """ Creates a GDataframe from a pandas dataframe of region and a pandas dataframe of metadata
+
+    :param regs: a pandas Dataframe of regions that is coherent with the GMQL data model
+    :param meta: (optional) a pandas Dataframe of metadata that is coherent with the regions
+    :param chr_name: (optional) which column of :attr:`~.regs` is the chromosome
+    :param start_name: (optional) which column of :attr:`~.regs` is the start
+    :param stop_name: (optional) which column of :attr:`~.regs` is the stop
+    :param strand_name: (optional) which column of :attr:`~.regs` is the strand
+    :return: a GDataframe
+    """
+    regs = check_regs(regs, chr_name, start_name, stop_name, strand_name)
+    regs = to_gmql_regions(regs)
+    if meta is not None:
+        if not check_meta(meta, regs):
+            raise ValueError("Error. Meta dataframe is not GMQL standard")
+    else:
+        meta = empty_meta(regs)
+    return GDataframe(regs, meta)
+
+
+def check_regs(region_df, chr_name=None, start_name=None, stop_name=None, strand_name=None):
+    """ Modifies a region dataframe to be coherent with the GMQL data model
+
+    :param region_df: a pandas Dataframe of regions that is coherent with the GMQL data model
+    :param chr_name: (optional) which column of :attr:`~.region_df` is the chromosome
+    :param start_name: (optional) which column of :attr:`~.region_df` is the start
+    :param stop_name: (optional) which column of :attr:`~.region_df` is the stop
+    :param strand_name: (optional) which column of :attr:`~.region_df` is the strand
+    :return: a modified pandas Dataframe
+    """
+    region_df = search_column(region_df,  chr_aliases, chr_types, 'chr', chr_name)
+    region_df = search_column(region_df, start_aliases, start_types, 'start', start_name)
+    region_df = search_column(region_df, stop_aliases, stop_types, 'stop', stop_name)
+    region_df = search_column(region_df, strand_aliases, strand_types, 'strand', strand_name)
+    return region_df
+
+
+def search_column(region_df, names, types, subs, name=None):
+    columns = region_df.columns.map(str.lower)
+    names = list(map(str.lower, names))
+
+    if name is not None:
+        if check_type(region_df[name], types):
+            region_df = region_df.rename({name: subs})
+        else:
+            raise TypeError("Column {} is not of type {}.".format(name, types))
+
+    isok = False
+    for e in columns:
+        if e in names and check_type(region_df[name], types):
+            region_df = region_df.rename({e: subs})
+            isok = True
+            break
+    if (not isok) and (subs != 'strand'):
+        raise ValueError("{} column was not found".format(subs))
+    return region_df
+
+
+def check_type(column, types):
+    for t in types:
+        if isinstance(column.dtype, t):
+            return True
+    return False
+
+
+def check_meta(meta_df, regs_df):
+    return True
+
+
+def empty_meta(regs):
+    index = regs.index.unique()
+    return pd.DataFrame(index=index)
+
+
+def to_gmql_regions(regs):
+    cols = ['chr', 'start', 'stop']
+    if 'strand' in regs.columns:
+        cols.append('strand')
+    cols.extend([c for c in regs.columns if c not in cols])
+    return regs[cols]
