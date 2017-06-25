ac22746c6ec3d31e4528294a61dc44c143abd5f7
lucananni93 <luca.nanni10@studio.unibo.it>
2017-06-25 19:29:41 +0200

    fixed bug (added self.index in conditions)
    Former-commit-id: e01baf060f102bb37802a6439732862af5b91641 [formerly 3a4af1a7c75792798dac9d6e21b20362ce0171d1] [formerly 6c7ad4c2e2f6e3d4a635fa6b88fda9391a6af7da]
    Former-commit-id: 173a77a816a74a0297ac40c2563a1a37de19b6db
    Former-commit-id: 546ecae8edf295dabd7b6baf905b8ac9dd6d73ba

--- gmql/dataset/DataStructures/MetaField.py
+++ gmql/dataset/DataStructures/MetaField.py
@@ -10,7 +10,7 @@ class MetaField:
         self.name = name
         self.metaCondition = meta_condition
         pymg = get_python_manager()
-        self.exp_build = pymg.getNewExpressionBuilder(self.index) # index = whatever
+        self.exp_build = pymg.getNewExpressionBuilder(self.index)
         
         if not (name.startswith("(") and name.endswith(")")) and meNode is None:
             self.meNode = self.exp_build.getMENode(name)
@@ -29,49 +29,49 @@ class MetaField:
     def __eq__(self, other):
         new_name = '(' + self.name + ' == ' + str(other) + ')'
         predicate = self.exp_build.createMetaPredicate(self.name, "EQ", str(other))
-        return MetaField(name=new_name, meta_condition=predicate)
+        return MetaField(name=new_name, meta_condition=predicate, index=self.index)
 
     def __ne__(self, other):
         new_name = '(' + self.name + ' != ' + str(other) + ')'
         predicate = self.exp_build.createMetaPredicate(self.name, "NOTEQ", str(other))
-        return MetaField(name=new_name, meta_condition=predicate)
+        return MetaField(name=new_name, meta_condition=predicate, index=self.index)
 
     def __gt__(self, other):
         new_name = '(' + self.name + ' > ' + str(other) + ')'
         predicate = self.exp_build.createMetaPredicate(self.name, "GT", str(other))
-        return MetaField(name=new_name, meta_condition=predicate)
+        return MetaField(name=new_name, meta_condition=predicate, index=self.index)
 
     def __ge__(self, other):
         new_name = '(' + self.name + ' >= ' + str(other) + ')'
         predicate = self.exp_build.createMetaPredicate(self.name, "GTE", str(other))
-        return MetaField(name=new_name, meta_condition=predicate)
+        return MetaField(name=new_name, meta_condition=predicate, index=self.index)
 
     def __lt__(self, other):
         new_name = '(' + self.name + ' < ' + str(other) + ')'
         predicate = self.exp_build.createMetaPredicate(self.name, "LT", str(other))
-        return MetaField(name=new_name, meta_condition=predicate)
+        return MetaField(name=new_name, meta_condition=predicate, index=self.index)
 
     def __le__(self, other):
         new_name = '(' + self.name + ' <= ' + str(other) + ')'
         predicate = self.exp_build.createMetaPredicate(self.name, "LTE", str(other))
-        return MetaField(name=new_name, meta_condition=predicate)
+        return MetaField(name=new_name, meta_condition=predicate, index=self.index)
     """
         CONDITIONS
     """
     def __and__(self, other):
         new_name = '(' + self.name + ' and ' + other.name + ')'
         condition = self.exp_build.createMetaBinaryCondition(self.metaCondition, "AND", other.metaCondition)
-        return MetaField(name=new_name, meta_condition=condition)
+        return MetaField(name=new_name, meta_condition=condition, index=self.index)
 
     def __or__(self, other):
         new_name = '(' + self.name + ' or ' + other.name + ')'
         condition = self.exp_build.createMetaBinaryCondition(self.metaCondition, "OR", other.metaCondition)
-        return MetaField(name=new_name, meta_condition=condition)
+        return MetaField(name=new_name, meta_condition=condition, index=self.index)
 
     def __invert__(self):
         new_name = 'not (' + self.name + ')'
         condition = self.exp_build.createMetaUnaryCondition(self.metaCondition, "NOT")
-        return MetaField(name=new_name, meta_condition=condition)
+        return MetaField(name=new_name, meta_condition=condition, index=self.index)
 
     """
         EXPRESSIONS
