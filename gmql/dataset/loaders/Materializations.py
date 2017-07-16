44f5ce406c8e0836e025005af12ab1af32b0fb55
lucananni93 <luca.nanni10@studio.unibo.it>
2017-07-16 16:00:00 +0200

    Moved multiple materializations to autonomous file

--- gmql/dataset/loaders/Materializations.py
+++ gmql/dataset/loaders/Materializations.py
@@ -1,52 +1,33 @@
-import pandas as pd
-from tqdm import tqdm
-import logging
-import pyspark
-from ..DataStructures.MetaRecord import MetaRecord
-
-logger = logging.getLogger('gmql_logger')
+from .. import GMQLDataset
 
 
-def meta_rdd_to_pandas(meta):
-    # make an rdd of dictionaries
-    meta = meta.map(lambda t: {'id_sample': t[0], t[1][0]: t[1][1]})
+def materialize(datasets):
+    """ Multiple materializations. Enables the user to specify a set of GMQLDataset to be materialized.
+    The engine will perform all the materializations at the same time, if an output path is provided,
+    while will perform each operation separately if the output_path is not specified.
 
-    # collect everything
-    meta = meta.collect()   # [{'id_sample': id, attr_name: value}, ...]
-
-    # turn into a dataframe
-    df = pd.DataFrame.from_dict(meta)
-    columns = df.columns
-
-    # grouping by 'id_sample'
-    g = df.groupby('id_sample')
-
-    logger.info("dataframe construction")
-    result_df = pd.DataFrame()
-    for col in tqdm(columns, total=len(columns)):
-        if col != 'id_sample':
-            result_df[col] = g[col].apply(to_list)
-    return result_df
-
-
-def to_list(x):
-    l = list(x)
-    l = [a for a in l if not pd.isnull(a)]
-    return l
-
-
-def pandas_to_rdd(df):
-    sc = pyspark.SparkContext.getOrCreate()
-    d = df.to_dict('index')
-    items = d.items()     # [(id, {attr_name: value, ...})]
-    items = list(
-        map(lambda x: (x[0], create_meta_record(x[1])), items)
-    )
-    rdd = sc.parallelize(items)   # RDD[(id, MetaRecord)]
-    return rdd
-
-
-def create_meta_record(dictionary):
-    meta_record = MetaRecord()
-    meta_record.from_dict(dictionary)
-    return meta_record
+    :param datasets: it can be a list of GMQLDataset or a dictionary {'output_path' : GMQLDataset}
+    :return: a list of GDataframe or a dictionary {'output_path' : GDataframe}
+    """
+    result = None
+    if isinstance(datasets, dict):
+        result = dict()
+        for output_path in datasets.keys():
+            dataset = datasets[output_path]
+            if not isinstance(dataset, GMQLDataset.GMQLDataset):
+                raise TypeError("The values of the dictionary must be GMQLDataset."
+                                " {} was given".format(type(dataset)))
+            gframe = dataset.materialize(output_path)
+            result[output_path] = gframe
+    elif isinstance(datasets, list):
+        result = []
+        for dataset in datasets:
+            if not isinstance(dataset, GMQLDataset.GMQLDataset):
+                raise TypeError("The values of the list must be GMQLDataset."
+                                " {} was given".format(type(dataset)))
+            gframe = dataset.materialize()
+            result.append(gframe)
+    else:
+        raise TypeError("The input must be a dictionary of a list. "
+                        "{} was given".format(type(datasets)))
+    return result
