92dffd0712fe3e684aa31ca105c79b8bdda46014
anilbey <tuncel.manil@gmail.com>
2017-07-28 18:03:05 +0200

    missing value imputation methods
    Former-commit-id: 6cb77ef08c937db67fe025d1fc20d46233bdeaa9 [formerly f9b8e36113f11db89c90db3737b1b81e59bc6bdb] [formerly 3e57ae835dcbe1cedc0721f9de997900337d2540]
    Former-commit-id: 7ed41a116860e473a15d7f9d29b7026c8b020784
    Former-commit-id: 554e8195eec2339632c96e01a179192569966d8d

--- gmql/ml/algorithms/preprocessing.py
+++ gmql/ml/algorithms/preprocessing.py
@@ -1,4 +1,5 @@
-
+from fancyimpute import KNN, SimpleFill, IterativeSVD
+import pandas as pd
 
 class Preprocessing:
     """
@@ -38,3 +39,47 @@ class Preprocessing:
         mask = (df.isnull().sum() / df.shape[0]).map(lambda x: True if x < percentage else False)
         pruned_df = df[df.columns[mask.values]]
         return pruned_df
+
+    @staticmethod
+    def impute_using_statistics(df, method='min'):
+        """
+        Imputes the missing values by the selected statistical property of each column
+        :param df: The input dataframe that contains missing values
+        :param method: The imputation method (min by default)
+            "zero": fill missing entries with zeros
+            "mean": fill with column means
+            "median" : fill with column medians
+            "min": fill with min value per column
+            "random": fill with gaussian noise according to mean/std of column
+        :return: the imputed dataframe
+        """
+        sf = SimpleFill(method)
+        imputed_matrix = sf.complete(df.values)
+        imputed_df = pd.DataFrame(imputed_matrix, df.index, df.columns)
+        return imputed_df
+
+    @staticmethod
+    def impute_knn(df, k=3):
+        """
+        Nearest neighbour imputations which weights samples using the mean squared difference on features for which two rows both have observed data.
+        :param df: The input dataframe that contains missing values
+        :param k: The number of neighbours
+        :return: the imputed dataframe
+        """
+        imputed_matrix = KNN(k=k).complete(df.values)
+        imputed_df = pd.DataFrame(imputed_matrix, df.index, df.columns)
+        return imputed_df
+
+    @staticmethod
+    def impute_svd(df, rank=10, convergence_threshold=0.00001, max_iters=200):
+        """
+        Imputes the missing values by using SVD decomposition
+        :param df:The input dataframe that contains missing values
+        :param rank: Rank value of the truncated SVD
+        :param convergence_threshold: The threshold to stop the iterations
+        :param max_iters: Max number of iterations
+        :return: the imputed dataframe
+        """
+        imputed_matrix = IterativeSVD(rank,convergence_threshold, max_iters).complete(df.values)
+        imputed_df = pd.DataFrame(imputed_matrix, df.index, df.columns)
+        return imputed_df
