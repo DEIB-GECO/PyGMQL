ca1fcc54e0d8a450a559096b2c232f78349cbeda
lucananni93 <luca.nanni10@studio.unibo.it>
2017-09-08 01:44:52 +0200

    Added source table feature
    Former-commit-id: 3b111573dd47d6be8e9021f9ecbed6cb6980fdea [formerly 9869c19c6a152e28880972b0c0c377e9976ac1a3] [formerly f4e922a237b384d3627f29dcb635714f0e59dd32]
    Former-commit-id: d60100b1e02f3295985cd46c6da740c88aaad067
    Former-commit-id: 310c655fdb9e256a7adf97a5c12099a6bffabd42

---
+++ gmql/dataset/loaders/Sources.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+LOCAL = "local"
+REMOTE = "remote"
+
+
+class SourcesTable:
+    """ Handles the location of the datasets during the program execution.
+    Manages both local and remote datasets. It is used when a mode change is
+    detected to send or download data sources to/from the remote server.
+    """
+    def __init__(self):
+        self.table = pd.DataFrame(columns=[LOCAL, REMOTE])
+        self.id_count = 0
+
+    def add_source(self, local=None, remote=None):
+        self.table.loc[self.id_count] = {LOCAL: local, REMOTE: remote}
+        result = self.id_count
+        self.id_count += 1
+        return result
+
+    def get_source(self, id):
+        res = self.table.loc[id]
+        return res[LOCAL], res[REMOTE]
+
+    def modify_source(self, id, local=None, remote=None):
+        if local is not None:
+            self.table.loc[id][LOCAL] = local
+        if remote is not None:
+            self.table.loc[id][REMOTE] = remote
+
+    def search_source(self, local=None, remote=None):
+        if local is not None:
+            ids = self.table[self.table[LOCAL] == local].index.tolist()
+        elif remote is not None:
+            ids = self.table[self.table[REMOTE] == remote].index.tolist()
+        else:
+            raise ValueError("local or remote must be given")
+        return ids[0] if len(ids) > 0 else None
