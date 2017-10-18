000e07457be5633397b5e2cf22072e12a9ab1a58
lucananni93 <luca.nanni10@studio.unibo.it>
2017-10-18 16:54:14 +0200

    added support for SQRT
    Former-commit-id: 65076c6664af136fdd32ca6a2a6416673f54c633 [formerly a48661f19d97667800e45154ac67e842783bc393] [formerly cc0187bf51d3d0c1b00a68e93d3df1f58f95bd04]
    Former-commit-id: 752da17d9e5df45ebc5fd1b7d751410af26ebb53
    Former-commit-id: e15de4ac3cbed24f9689b028d899e4a01b6786a1

---
+++ gmql/dataset/DataStructures/ExpressionNodes.py
@@ -0,0 +1,23 @@
+from .MetaField import MetaField
+from .RegField import RegField
+
+
+def SQRT(argument):
+    """ Computes the square matrix of the argument
+
+    :param argument: a dataset region field (dataset.field) or metadata (dataset['field'])
+    :return:
+    """
+    if isinstance(argument, MetaField):
+        new_name = "SQRT({})".format(argument.name)
+        new_node = argument.exp_build.getUnaryMetaExpression(argument.meNode, "SQRT")
+        result = MetaField(name=new_name, index=argument.index, meNode=new_node)
+    elif isinstance(argument, RegField):
+        new_name = "SQRT({})".format(argument.name)
+        new_node = argument.exp_build.getUnaryRegionExpression(argument.reNode, "SQRT")
+        result = RegField(name=new_name, index=argument.index, reNode=new_node)
+    else:
+        raise TypeError("You have to give as input a RegField (dataset.field)"
+                        "or a MetaField (dataset['field']")
+    return result
+
