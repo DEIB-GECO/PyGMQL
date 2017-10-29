3b838cd09ef1746d055a88286994306cda8e440c
lucananni93 <luca.nanni10@studio.unibo.it>
2017-10-29 15:03:23 +0100

    code restructure
    Former-commit-id: 5c384dab72895d10dbd713970c4ce0fdb422fd6e [formerly 0d1a28019c51e8d3f31ab61d80cc971e4f61a987] [formerly a4775b2022931c11466861783a15a3c9ac0546da]
    Former-commit-id: b7668d4b0f4df02b824ee68413c5ee4e807f8f83
    Former-commit-id: 982c1d410879bfb6693673123c91cb75499472e0

--- gmql/dataset/DataStructures/ExpressionNodes.py
+++ gmql/dataset/DataStructures/ExpressionNodes.py
@@ -8,15 +8,10 @@ def SQRT(argument):
     :param argument: a dataset region field (dataset.field) or metadata (dataset['field'])
     """
     if isinstance(argument, MetaField):
-        new_name = "SQRT({})".format(argument.name)
-        new_node = argument.exp_build.getUnaryMetaExpression(argument.meNode, "SQRT")
-        result = MetaField(name=new_name, index=argument.index, meNode=new_node)
+        return argument._unary_expression("SQRT")
     elif isinstance(argument, RegField):
-        new_name = "SQRT({})".format(argument.name)
-        new_node = argument.exp_build.getUnaryRegionExpression(argument.reNode, "SQRT")
-        result = RegField(name=new_name, index=argument.index, reNode=new_node)
+        return argument._unary_expression("SQRT")
     else:
         raise TypeError("You have to give as input a RegField (dataset.field)"
                         "or a MetaField (dataset['field']")
-    return result
 
