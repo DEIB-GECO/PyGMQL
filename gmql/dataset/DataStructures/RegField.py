976661fa9c297c4772c890df87a87e8a41712477
lucananni93 <luca.nanni10@studio.unibo.it>
2017-10-29 15:04:41 +0100

    huge code restructuring + metafields in reg_projects
    Former-commit-id: 92c5fdc9d1d05558b9b0b5ade430a29c83bcffbc [formerly 1eff49e2e760637b650b69288921345ed5952bce] [formerly 285971489f1631f82d177730580776463d4d7873]
    Former-commit-id: 2060f925ca5b607fa01d12738bbf121cf03ff177
    Former-commit-id: 4e63a777d623683ae0f7e49a5cbb64e936d191a8

--- gmql/dataset/DataStructures/RegField.py
+++ gmql/dataset/DataStructures/RegField.py
@@ -1,6 +1,7 @@
 from ... import get_python_manager
 from .MetaField import MetaField
 
+
 class RegField:
 
     def __init__(self, name, index=None, region_condition=None, reNode=None):
@@ -17,6 +18,9 @@ class RegField:
         else:
             self.reNode = reNode
 
+    def is_complex(self):
+        return self.name.startswith("(") and self.name.endswith(")")
+
     def getRegionCondition(self):
         return self.region_condition
 
@@ -28,39 +32,27 @@ class RegField:
     """
 
     def __eq__(self, other):
-        new_name = '(' + self.name + ' == ' + str(other) + ')'
-        other = _process_other(other)
-        predicate = self.exp_build.createRegionPredicate(self.name, "EQ", other)
-        return RegField(name=new_name, region_condition=predicate, index=self.index)
+        return self._predicate(other, "EQ")
 
     def __ne__(self, other):
-        new_name = '(' + self.name + ' != ' + str(other) + ')'
-        other = _process_other(other)
-        predicate = self.exp_build.createRegionPredicate(self.name, "NOTEQ", other)
-        return RegField(name=new_name, region_condition=predicate, index=self.index)
+        return self._predicate(other, "NOTEQ")
 
     def __gt__(self, other):
-        new_name = '(' + self.name + ' > ' + str(other) + ')'
-        other = _process_other(other)
-        predicate = self.exp_build.createRegionPredicate(self.name, "GT", other)
-        return RegField(name=new_name, region_condition=predicate, index=self.index)
+        return self._predicate(other, "GT")
 
     def __ge__(self, other):
-        new_name = '(' + self.name + ' >= ' + str(other) + ')'
-        other = _process_other(other)
-        predicate = self.exp_build.createRegionPredicate(self.name, "GTE", other)
-        return RegField(name=new_name, region_condition=predicate, index=self.index)
+        return self._predicate(other, "GTE")
 
     def __lt__(self, other):
-        new_name = '(' + self.name + ' < ' + str(other) + ')'
-        other = _process_other(other)
-        predicate = self.exp_build.createRegionPredicate(self.name, "LT", other)
-        return RegField(name=new_name, region_condition=predicate, index=self.index)
+        return self._predicate(other, "LT")
 
     def __le__(self, other):
-        new_name = '(' + self.name + ' <= ' + str(other) + ')'
-        other = _process_other(other)
-        predicate = self.exp_build.createRegionPredicate(self.name, "LTE", other)
+        return self._predicate(other, "LTE")
+
+    def _predicate(self, other, operator):
+        new_name = '(' + self.name + operator + str(other) + ')'
+        other = self._process_other(other)
+        predicate = self.exp_build.createRegionPredicate(self.name, operator, other)
         return RegField(name=new_name, region_condition=predicate, index=self.index)
 
     """
@@ -68,18 +60,25 @@ class RegField:
     """
 
     def __and__(self, other):
-        new_name = '(' + self.name + ' and ' + other.name + ')'
-        condition = self.exp_build.createRegionBinaryCondition(self.region_condition, "AND", other.region_condition)
-        return RegField(name=new_name, region_condition=condition, index=self.index)
+        return self._binary_condition(other, "AND")
 
     def __or__(self, other):
-        new_name = '(' + self.name + ' or ' + other.name + ')'
-        condition = self.exp_build.createRegionBinaryCondition(self.region_condition, "OR", other.region_condition)
-        return RegField(name=new_name, region_condition=condition, index=self.index)
+        return self._binary_condition(other, "OR")
 
     def __invert__(self):
-        new_name = 'not (' + self.name + ')'
-        condition = self.exp_build.createRegionUnaryCondition(self.region_condition, "NOT")
+        return self._unary_condition("NOT")
+
+    def _binary_condition(self, other, operator):
+        if isinstance(other, RegField) and other.region_condition is not None:
+            new_name = '(' + self.name + operator + other.name + ')'
+            condition = self.exp_build.createRegionBinaryCondition(self.region_condition, operator, other.region_condition)
+            return RegField(name=new_name, region_condition=condition, index=self.index)
+        else:
+            raise TypeError("Binary conditions can be done only between RegFields conditions")
+
+    def _unary_condition(self, operator):
+        new_name = operator + '(' + self.name + ')'
+        condition = self.exp_build.createRegionUnaryCondition(self.region_condition, operator)
         return RegField(name=new_name, region_condition=condition, index=self.index)
 
     def isin(self, values):
@@ -108,98 +107,89 @@ class RegField:
         EXPRESSIONS
     """
 
-    def __get_return_type(self, other):
-        if isinstance(other, str):
-            other = self.exp_build.getREType("string", other)
-        elif isinstance(other, int):
-            other = self.exp_build.getREType("int", str(other))
-        elif isinstance(other, float):
-            other = self.exp_build.getREType("float", str(other))
-        else:
-            raise ValueError("Expected string, float or integer. {} was found".format(type(other)))
-        return other
-
     def __add__(self, other):
-        if isinstance(other, RegField):
-            # we are dealing with an other RegField
-            new_name = '(' + self.name + ' + ' + other.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "ADD", other.reNode)
-        else:
-            other = self.__get_return_type(other)
-            new_name = '(' + self.name + ' + ' + str(other) + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "ADD", other)
-        return RegField(name=new_name, index=self.index, reNode=node)
+        return self._binary_expression(other, "ADD")
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __sub__(self, other):
-        if isinstance(other, RegField):
-            # we are dealing with an other RegField
-            new_name = '(' + self.name + ' - ' + other.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "SUB", other.reNode)
-        else:
-            other = self.__get_return_type(other)
-            new_name = '(' + self.name + ' - ' + str(other) + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "SUB", other)
-        return RegField(name=new_name, index=self.index, reNode=node)
+        return self._binary_expression(other, "SUB", order="left")
 
     def __rsub__(self, other):
-        if isinstance(other, RegField):
-            # we are dealing with an other RegField
-            new_name = '(' + other.name + ' - ' + self.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(other.reNode, "SUB", self.reNode)
-        else:
-            other = self.__get_return_type(other)
-            new_name = '(' + str(other) + ' - ' + self.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(other, "SUB", self.reNode)
-        return RegField(name=new_name, index=self.index, reNode=node)
+        return self._binary_expression(other, "SUB", order="right")
 
     def __mul__(self, other):
-        if isinstance(other, RegField):
-            # we are dealing with an other RegField
-            new_name = '(' + self.name + ' * ' + other.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "MUL", other.reNode)
-        else:
-            other = self.__get_return_type(other)
-            new_name = '(' + self.name + ' * ' + str(other) + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "MUL", other)
-        return RegField(name=new_name, index=self.index, reNode=node)
+        return self._binary_expression(other, "MUL")
 
     def __rmul__(self, other):
         return self.__mul__(other)
 
     def __truediv__(self, other):
-        if isinstance(other, RegField):
-            # we are dealing with an other RegField
-            new_name = '(' + self.name + ' / ' + other.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "DIV", other.reNode)
-        else:
-            other = self.__get_return_type(other)
-            new_name = '(' + self.name + ' / ' + str(other) + ')'
-            node = self.exp_build.getBinaryRegionExpression(self.reNode, "DIV", other)
-        return RegField(name=new_name, index=self.index, reNode=node)
+        return self._binary_expression(other, "DIV", order="left")
 
     def __rtruediv__(self, other):
-        if isinstance(other, RegField):
-            # we are dealing with an other RegField
-            new_name = '(' + other.name + ' / ' + self.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(other.reNode, "DIV", self.reNode)
+        return self._binary_expression(other, "DIV", order="right")
+
+    def __neg__(self):
+        return self._unary_expression("NEG")
+
+    def __get_return_type(self, other):
+        if isinstance(other, str):
+            other_name = other
+            other = self.exp_build.getREType("string", other)
+        elif isinstance(other, int):
+            other_name = str(other)
+            other = self.exp_build.getREType("int", str(other))
+        elif isinstance(other, float):
+            other_name = str(other)
+            other = self.exp_build.getREType("float", str(other))
+        elif isinstance(other, RegField):
+            other_name = other.name
+            other = other.reNode
+        elif isinstance(other, MetaField):
+            if other.index != self.index:
+                raise ValueError("You cannot mix fields or attributes of different"
+                                 "dataset in a condition or expression")
+            if other.reMetaNode is not None:
+                other_name = other.name
+                other = other.reMetaNode
+            else:
+                raise ValueError("You cannot use a complex Metadata Expression in a "
+                                 "region expression. Only MetaFields are allowed.")
         else:
-            other = self.__get_return_type(other)
-            new_name = '(' + str(other) + ' / ' + self.name + ')'
-            node = self.exp_build.getBinaryRegionExpression(other, "DIV", self.reNode)
+            raise ValueError("Expected string, float, integer, RegField or MetaField"
+                             ". {} was found".format(type(other)))
+        return other, other_name
+
+    def _binary_expression(self, other, operation, order="left"):
+        other, other_name = self.__get_return_type(other)
+        if order == 'left':
+            new_name = '(' + self.name + operation + other_name + ')'
+            node = self.exp_build.getBinaryRegionExpression(self.reNode, operation, other)
+        else:
+            new_name = '(' + other_name + operation + self.name + ')'
+            node = self.exp_build.getBinaryRegionExpression(other, operation, self.reNode)
         return RegField(name=new_name, index=self.index, reNode=node)
 
+    def _unary_expression(self, operation):
+        new_name = operation + "(" + self.name + ")"
+        node = self.exp_build.getUnaryRegionExpression(self.reNode, operation)
+        return RegField(name=new_name, index=self.index, reNode=node)
 
-def _process_other(other):
-    if isinstance(other, MetaField):
-        return other.metaAccessor
-    elif isinstance(other, str):
-        return other
-    elif isinstance(other, int) or \
-         isinstance(other, float):
-        return str(other)
-    else:
-        raise TypeError("Other is of unknown type. "
-                        "{} provided".format(type(other)))
+    def _process_other(self, other):
+        if isinstance(other, MetaField):
+            if other.index != self.index:
+                raise ValueError("You cannot mix fields or attributes of different"
+                                 "dataset in a condition or expression")
+            if other.metaAccessor is not None:
+                return other.metaAccessor
+            else:
+                raise ValueError("When mixing RegFields with metadata you can only use "
+                                 "MetaFields of a metadata attribute and "
+                                 "not complex conditions or expressions on metadata")
+        elif isinstance(other, (str, int, float)):
+            return str(other)
+        else:
+            raise TypeError("Other is of unknown type. "
+                            "{} provided".format(type(other)))
