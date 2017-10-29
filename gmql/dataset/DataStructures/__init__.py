d54fc562de9ae18b0e30adf66be5ca998e83aaca
lucananni93 <luca.nanni10@studio.unibo.it>
2017-10-29 15:00:12 +0100

    added operator correspondences
    Former-commit-id: a163745ab6adc89d1816b3b139b862cfe24760bc [formerly e02bdedaacb168aa1264668a5a194520a6151fc9] [formerly 84f8790b45e04e5faaff2a48277bf5d31eaf1bbb]
    Former-commit-id: e787df842bb52186e261056f65152e17397f3053
    Former-commit-id: d009c83936cd48bf2f8f5081dfea7cd03f221db7

--- gmql/dataset/DataStructures/__init__.py
+++ gmql/dataset/DataStructures/__init__.py
@@ -5,3 +5,16 @@ start_aliases = ['start', 'left']
 stop_aliases = ['stop', 'right']
 strand_aliases = ['str', 'strand']
 id_sample_aliases = ['id_sample', 'sample_id', 'sample']
+
+operator_correspondences = {
+    'LT': 'GT',
+    'GT': 'LT',
+    'LTE': 'GTE',
+    'GTE': 'LTE',
+    'EQ': 'EQ',
+    'NOTEQ': 'NOTEQ'
+}
+
+
+def _get_opposite_operator(operator):
+    return operator_correspondences[operator]
