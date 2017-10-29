c44ba830a7b5b0575294ce6a22d0b94aee6375d5
lucananni93 <luca.nanni10@studio.unibo.it>
2017-10-29 14:59:50 +0100

    removed ancient useless stuff
    Former-commit-id: c5e9d5197b6f63db6e989be5b35edf4a5806b718 [formerly 4c99ddaec28b4aadcbc7501749c305c5108f6440] [formerly 7a7c00fc294941cfb2a509602cd8a7fe668c6b21]
    Former-commit-id: a94792f09c771a155bd4792e67bdc58286c2dbe7
    Former-commit-id: 480c6474f39c9cb31a9816ec68dcf076f354706a

--- gmql/dataset/loaders/__init__.py
+++ gmql/dataset/loaders/__init__.py
@@ -1,29 +1,12 @@
 import os
 
 """
-Loader settings:
-we use the GMQL scala class CombineTextFileWithPathInputFormat in order to load the region and metadata files with the
-same id_sample based on the hash of the file name
-"""
-
-inputFormatClass = 'it.polimi.genomics.spark.implementation.loaders.Loaders$CombineTextFileWithPathInputFormat'
-keyFormatClass = 'org.apache.hadoop.io.LongWritable'
-valueFormatClass = 'org.apache.hadoop.io.Text'
-defaultCombineSize = 64
-
-# configuration of the Hadoop loader used by spark
-conf = {"textinputformat.record.delimiter": "\n",
-        "mapreduce.input.fileinputformat.input.dir.recursive": "true",
-        "mapred.max.split.size": str(defaultCombineSize*1024*1024)
-        }
-
-
-"""
     Generation of the index of the pandas dataframe.
     This can be done in different ways:
     - hashing the complete file name
     - using directly the file as index (this is visually appealing :) )
 """
+
 
 def generateHashKey(filename):
     return hash(filename)
