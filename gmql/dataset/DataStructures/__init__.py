fce3196e6fa8d7d669ab07c2bc94a86c1f6a1128
lucananni93 <lucananni93@gmail.com>
2017-12-23 01:07:21 +0100

    added 'end' keyword
    Former-commit-id: 2f4fc916755479b11922b6fc05c5ffa6952fe836 [formerly 322dcd84546ffdc3c7e2680b89f59190bbb6eea5] [formerly 73244e9a36e0b7cdddd591bfa1e8a08b66717c1e]
    Former-commit-id: c44d8fac5ae092efcd25f7a454c8cfd7d7a415fb
    Former-commit-id: e8814f6ad24965fe18973250d57a7bf8b48a5a72

--- gmql/dataset/DataStructures/__init__.py
+++ gmql/dataset/DataStructures/__init__.py
@@ -2,7 +2,7 @@ reg_fixed_fileds = ['chr', 'start', 'stop', 'strand']
 
 chr_aliases = ['chr', 'chrom', 'chromosome']
 start_aliases = ['start', 'left']
-stop_aliases = ['stop', 'right']
+stop_aliases = ['stop', 'right', 'end']
 strand_aliases = ['str', 'strand']
 id_sample_aliases = ['id_sample', 'sample_id', 'sample']
 
