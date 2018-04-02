7c3b90d7f7db84588d21af37f55296596a9693b7
lucananni93 <lucananni93@gmail.com>
2018-04-02 18:50:50 +0200

    support 2.7

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(name='gmql',
             'Programming Language :: Python :: 3'
       ],
       keywords="genomics big data pandas python",
-      python_requires='>=2.7',
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
       include_package_data=True,
       zip_safe=False)
