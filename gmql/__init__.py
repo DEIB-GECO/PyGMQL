6987a65b86f94c5df1a98d09d5aef46fa0b1b9ec
lucananni93 <lucananni93@gmail.com>
2018-04-27 20:38:32 +0200

    added get_remote_manager to top level functions

--- gmql/__init__.py
+++ gmql/__init__.py
@@ -11,7 +11,7 @@ from .settings import init_settings as __init_settings
 from .managers import init_managers as __init_managers
 from .settings import set_remote_address, set_meta_profiling, set_mode, set_progress
 from .managers import login, logout, get_remote_address, get_session_manager, set_backend_path, set_py4j_path, \
-    get_configuration
+    get_configuration, get_remote_manager
 
 __init_settings()
 __init_managers()
